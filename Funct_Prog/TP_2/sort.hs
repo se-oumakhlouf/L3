e1dfb772888a590ca0c8ac195bbe9f274beff0f8
selym <selym@selym.myguest.virtualbox.org>
2024-02-13 19:34:41 +0100

    TP_2 Funct Prog end

--- Funct_Prog/TP_2/sort.hs
+++ Funct_Prog/TP_2/sort.hs
@@ -1,5 +1,42 @@
 -- Question a
 
--- msplit :: [a] -> [[a]]
-msplit []       = []
-msplit (x : xs) = 
+-- msplit [] > ([] ,[])
+-- msplit [1,2,3,4] > ([1, 3], [2, 4])
+-- msplit :: [a] -> ([a], [a])
+msplit [] = ([], [])
+msplit [x] = ([x], [])
+msplit (x : y : xs) = (x : zs, y : ws)
+  where
+    (zs, ws) = msplit xs
+
+
+-- Question b
+
+-- merge [] [1, 3, 4, 5] > [1, 3, 4, 5]
+-- merge [1, 2, 3, 4] [] > [1, 2, 3, 4]
+-- merge [1, 3, 4, 5] [2, 6, 7, 8] > [1, 2, 3, 4, 5, 6, 7, 8]
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y : ys)
+  | x <= y      = x : merge xs (y : ys)
+  | otherwise   = y : merge (x : xs) ys
+
+
+-- Question c
+
+-- mergeSort [1, 3, 4, 5, 2, 6, 7, 8] > [1, 2, 3, 4, 5, 6, 7, 8]
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort zs) (mergeSort ws)
+  where
+    (zs, ws) = msplit xs
+
+
+-- Question d
+
+-- quickSort [1, 3, 4, 5, 2, 6, 7, 8] > [1, 2, 3, 4, 5, 6, 7, 8]
+quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort (x : xs) = quickSort [y | y <- xs, y <= x] ++ [x] ++ quickSort [y | y <- xs, y > x]
