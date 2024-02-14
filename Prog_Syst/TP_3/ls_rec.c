e86ef6344fb8e99de5c6e0b2fccc94dda5946e33
selym <selym@selym.myguest.virtualbox.org>
2024-02-14 23:03:41 +0100

    TP_3 n°1 done

--- Prog_Syst/TP_3/ls_rec.c
+++ Prog_Syst/TP_3/ls_rec.c
@@ -4,7 +4,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void process_path(char* pathname) {
+void process_dir(char* pathname, int recursive);
+
+void process_path(char* pathname, int recursive) {
     struct stat buf;
     int size;
     char* restrict readbuf = NULL;
@@ -15,29 +17,32 @@ void process_path(char* pathname) {
     printf("Last Modification: %ld\n", buf.st_mtime);
     printf("File type: ");
     if (S_ISREG(buf.st_mode))           printf("f\n");
-    else if (S_ISDIR(buf.st_mode))      printf("d\n");
+    else if (S_ISDIR(buf.st_mode)) {
+        printf("d\n");
+        if (recursive) process_dir(pathname, recursive);
+    }
     else if (S_ISLNK(buf.st_mode)) {
         printf("l\n");
         try(readbuf = malloc(buf.st_size + 2), NULL);
         size = try(readlink(pathname, readbuf, buf.st_size + 2), -1);
         readbuf[size] = '\0';
         printf("Linked to : %s\n", readbuf);
-        process_path(readbuf);
+        process_path(readbuf, recursive);
     }
     else                                printf("?\n");
     free(readbuf);
 }
 
-void process_dir(char* pathname) {
+void process_dir(char* pathname, int recursive) {
     DIR* dirp = try(opendir(pathname), NULL);
-    struct dirent* dirent = try(readdir(dirp), NULL);
-    printf("Name : %s\n", dirent->d_name);
+    struct dirent* dirent;
     while ((dirent = readdir(dirp))) {
+        if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0) continue;
         printf("\n || Name : %s ||\n", dirent->d_name);
         // build the full path
         char file_path[8192];
         snprintf(file_path, sizeof(file_path), "%s/%s", pathname, dirent->d_name);
-        process_path(file_path);
+        process_path(file_path, recursive);
     }
     try(closedir(dirp), -1);
 }
@@ -55,14 +60,32 @@ int is_file(const char* pathname) {
 }
 
 int main(int argc, char* argv[]) {
- 
-    if (argc == 1) {
-        process_dir(".");
-    } else {
-        for (int i = 1; i < argc; i++) {
-            if (is_file(argv[i]))           process_path(argv[i]);
-            else if (is_directory(argv[i])) process_dir(argv[i]);
-            if (i < argc - 1) printf("\n");
+
+    extern char *optarg;
+    extern int optind;
+    int opt;
+    int recusive = 0; // 0: false, 1: true
+
+    while((opt = getopt(argc, argv, "R")) != -1) {
+        switch (opt)
+        {
+        case 'R':
+            recusive = 1;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-R] [FILE]...\n", argv[0]);
+            exit(EXIT_FAILURE);
+            break;
         }
     }
+
+    if (argc == 1 || (argc == 2 && recusive)) {
+        process_dir(".", recusive);
+    } else {
+        for (char** argp = argv + optind; *argp; argp++) {
+            if (is_file(*argp))           process_path(*argp, recusive);
+            else if (is_directory(*argp)) process_dir(*argp, recusive);
+        }
+    }
+    exit(EXIT_SUCCESS);
 }
