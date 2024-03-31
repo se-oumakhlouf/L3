34a9ee8a6ed3463ef65a8422cf1c6deb466f2036
Selym <selymoumakhlouf@gmail.com>
2024-03-31 14:54:57 +0200

    Nouvelle structure

--- Compil/TP_1/Symbols_Table.c
+++ Compil/TP_1/Symbols_Table.c
@@ -5,104 +5,156 @@
 #include "Symbols_Table.h"
 #include "../../try.h"
 
+// using try might be a bad idea, stops the program if malloc fails
 
-Symbols_Table* init_table() {
-    Symbols_Table* table = (Symbols_Table*)malloc(sizeof(Symbols_Table));
-    table->size = 0;
-    table->size_max = INIT_SIZE;
-    return table;
+int determine_size(Type type) {
+    switch (type) {
+        case INT: return 4;
+        case CHAR: return 1;
+        case VOID: return 0;
+        // case FUNCTION: return ???;
+        default: return -1;
+    }
 }
 
-void free_table(Symbols_Table * table) {
-    for (int i = 0; i < table->size; i++) {
-        free(table->tab[i].ident);
-        free(table->tab[i].type);
+char* type_to_string(Type type) {
+    switch (type) {
+        case FUNCTION: return "FUNCTION";
+        case INT: return "INT";
+        case CHAR: return "CHAR";
+        case VOID: return "VOID";
+        default: return "UNKNOWN";
     }
-    free(table);
 }
 
-Symbols* make_symbols(char* ident, char* type, long int adresse, long int deplct) {
-    Symbols* symbol = (Symbols*)malloc(sizeof(Symbols));
-    if (symbol == NULL) {
-        return NULL;
-    }
-    try(symbol->ident = strdup(ident), NULL);
-    try(symbol->type = strdup(type), NULL);
-    symbol->adresse = adresse;
-    symbol->deplct = deplct;
+int get_last_adress(Symbols_Table* sym_table) {
+    if (sym_table->index == 0) {return 0;}
+    return sym_table->tab[sym_table->index - 1]->deplct;
+}
+
+Symbol* make_symbol(char* ident, Type type, int adress_prev) {
+    Symbol* symbol = try((Symbol*)malloc(sizeof(Symbol)), NULL);
+    symbol->ident = try(strdup(ident), NULL);
+    symbol->type = type;
+    symbol->size = determine_size(type);
+    symbol->deplct = adress_prev + symbol->size;
     return symbol;
 }
 
-void add_symbol(Symbols_Table *table, Symbols *symbol) {
-    if (isPresent(table, symbol)) {return;}
-    if (table->size < table->size_max) {
-        table->tab[table->size++] = *symbol;
-        printf("added : %s\n", symbol->ident);
-    } else {printf("The table is full\n");}
+Symbols_Table* init_Sym_table() {
+    Symbols_Table* sym_table = try((Symbols_Table*)malloc(sizeof(Symbols_Table)), NULL);
+    sym_table->index = 0;
+    memset(sym_table->tab, 0, INIT_SIZE * sizeof(Symbol*));
+    return sym_table;
 }
 
-int isEqual(Symbols s1, Symbols s2) {
-    if (strcmp(s1.ident, s2.ident) == 0) {return 1;} 
-    return 0;
+Function_Table* init_Func_table(char* ident) {
+    Function_Table* func_table = try((Function_Table*)malloc(sizeof(Function_Table)), NULL);
+    func_table->next = NULL;
+    func_table->ident = try(strdup(ident), NULL);
+    func_table->header = init_Sym_table();
+    func_table->body = init_Sym_table();
+    return func_table;
 }
 
-int isPresent(Symbols_Table *table, Symbols *symbol) {
-    for (int i = 0; i < table->size; i++) {
-        if (isEqual(table->tab[i], *symbol)) {
-            printf("Equal : %s, %s\n", table->tab[i].ident, symbol->ident);
+Program_Table* init_Program_table() {
+    Program_Table* prog_table = try((Program_Table*)malloc(sizeof(Program_Table)), NULL);
+    prog_table->globals = init_Sym_table();
+    prog_table->functions = NULL;
+    return prog_table;
+}
+
+void free_symbol(Symbol* symbol) {
+    free(symbol->ident);
+    free(symbol);
+}
+
+void free_Sym_table(Symbols_Table* sym_table) {
+    for (int i = 0; i < INIT_SIZE; i++) {
+        if (sym_table->tab[i] != NULL) {
+            free_symbol(sym_table->tab[i]);
+        }
+    }
+    free(sym_table);
+}
+
+void free_Func_table(Function_Table* func_table) {
+    free(func_table->ident);
+    free_Sym_table(func_table->header);
+    free_Sym_table(func_table->body);
+    free(func_table);
+}
+
+void free_Program_table(Program_Table* prog_table) {
+    free_Sym_table(prog_table->globals);
+    Function_Table* func = prog_table->functions;
+    while (func != NULL) {
+        Function_Table* next = func->next;
+        free_Func_table(func);
+        func = next;
+    }
+    free(prog_table);
+}
+
+int isPresent(Symbols_Table* sym_table, Symbol* symbol) {
+    for (int i = 0; i < sym_table->index; i++) {
+        if (strcmp(sym_table->tab[i]->ident, symbol->ident) == 0) {
             return 1;
         }
     }
     return 0;
 }
 
-void print_table(Symbols_Table *table) {
-    printf("\nSymbols Table:\n");
-    printf("%-10s %-10s %-10s %-10s\n", "Ident", "Type", "Adresse", "Deplct");
-    for (int i = 0; i < table->size; i++) {
-        printf("%-10s %-10s %-10ld %-10ld\n", table->tab[i].ident, table->tab[i].type, table->tab[i].adresse, table->tab[i].deplct);
-    }
-    printf("\n");
+void add_symbol(Symbols_Table* sym_table, Symbol* symbol) {
+    if (isPresent(sym_table, symbol)) {return;}
+    sym_table->tab[sym_table->index++] = symbol;
 }
 
-/*
-void read_globals(FILE *file, Symbols_Table *table) {
-    char line[256]; // Taille maximale d'une ligne dans le fichier source
-    while (fgets(line, sizeof(line), file) != NULL) {
-        // Ignorer les lignes vides
-        if (strlen(line) <= 1) {
-            continue;
-        }
-        // Ignorer les déclarations de fonctions
-        if (strstr(line, "int ") != NULL && strstr(line, "(") != NULL) {
-            continue;
-        }
-        // Ignorer la déclaration de la fonction main
-        if (strstr(line, "main") != NULL) {
-            continue;
-        }
-        // Identifier la déclaration de variable globale
-        char *identifier = strtok(line, " ;\n");
-        char *type = strtok(NULL, " ;\n");
-        if (identifier != NULL && type != NULL) {
-            Symbols *symbol = (Symbols*)malloc(sizeof(Symbols));
-            symbol->ident = strdup(identifier);
-            symbol->type = strdup(type);
-            symbol->adresse = 0; // Vous pouvez affecter une valeur appropriée à cette adresse
-            symbol->deplct = 0; // Vous pouvez affecter une valeur appropriée à ce déplacement
-            add_symbol(table, symbol);
-        }
+void print_symbol(Symbol* symbol) {
+    printf("\tIdent: %-10s\n", symbol->ident);
+    printf("\t\tType: %-10s\n", type_to_string(symbol->type));
+    printf("\t\tSize: %-10d\n", symbol->size);
+    printf("\t\tDeplct: %-10ld\n", symbol->deplct);
+}
+
+void print_sym_table(Symbols_Table* sym_table) {
+    for (int i = 0; i < sym_table->index; i++) {
+        print_symbol(sym_table->tab[i]);
     }
 }
-*/
+
+void print_func_table(Function_Table* func) {
+    printf("Ident: %-10s\n", func->ident);
+    printf("Header:\n");
+    print_sym_table(func->header);
+    printf("Body:\n");
+    print_sym_table(func->body);
+}
+
+void print_program_table(Program_Table* prog) {
+    printf("Globals:\n");
+    print_sym_table(prog->globals);
+    Function_Table* func = prog->functions;
+    while (func != NULL) {
+        print_func_table(func);
+        func = func->next;
+    }
+}
+
+
 int main(int argc, char* argv[]) {
-    Symbols_Table* table = init_table();
-    Symbols* s1 =  make_symbols("tab", "array", 1010, 1020);
-    Symbols* s2 =  make_symbols("tab2", "array", 10102020, 1020);
-    add_symbol(table, s1);
-    add_symbol(table, s2);
-    add_symbol(table, s1);
-    print_table(table);
-    free_table(table);
+    Program_Table* prog_table = init_Program_table();
+    Symbol* symbol = make_symbol("a", INT, get_last_adress(prog_table->globals));
+    add_symbol(prog_table->globals, symbol);
+    symbol = make_symbol("b", CHAR, get_last_adress(prog_table->globals));
+    add_symbol(prog_table->globals, symbol);
+    symbol = make_symbol("c", CHAR, get_last_adress(prog_table->globals));
+    add_symbol(prog_table->globals, symbol);
+    symbol = make_symbol("d", INT, get_last_adress(prog_table->globals));
+    add_symbol(prog_table->globals, symbol);
+    symbol = make_symbol("e", CHAR, get_last_adress(prog_table->globals));
+    add_symbol(prog_table->globals, symbol);
+    print_program_table(prog_table);
+    free_Program_table(prog_table);
     return 0;
 }
--- Compil/TP_1/Symbols_Table.h
+++ Compil/TP_1/Symbols_Table.h
@@ -1,38 +1,104 @@
 #ifndef __SYM__
 #define __SYM__
 
-#define INIT_SIZE 50
+#define INIT_SIZE 2048
 
-typedef struct Symbols
+typedef enum Type
 {
-    char* ident; 
-    char* type;
-    long int adresse; /* Son adresse */
-    long int deplct; /* Prochain emplacement mémoire */
-} Symbols;
+    FUNCTION,
+    INT,
+    CHAR,
+    VOID
+} Type;
+
+
+typedef struct Symbol
+{
+    char* ident;        /* name of the symbol */
+    Type type;          /* type of the symbol */
+    int size;           /* size (in bytes) based on the type */
+    long int deplct;    /* next space in memory */
+} Symbol;
+
 
 typedef struct Symbols_Table
 {
-    Symbols tab[INIT_SIZE];
-    int size;
-    int size_max;
+    Symbol* tab[INIT_SIZE];     /* array of symbols */
+    long int index;             /* index of the next symbol */
 } Symbols_Table;
 
 
-Symbols_Table* init_table();
+typedef struct Function_Table
+{
+    char* ident;                    /* name of the function */
+    Symbols_Table* header;          /* parameters */
+    Symbols_Table* body;            /* local variables */
+    struct Function_Table* next;    /* next function */
+} Function_Table;
 
-void free_table(Symbols_Table * table);
 
-Symbols* make_symbols(char* ident, char* type, long int adresse, long int deplct);
+typedef struct Program_Table
+{
+    Symbols_Table* globals;         /* global variables */
+    Function_Table* functions;      /* functions */
+} Program_Table;
+/* ↑ pointers are lighter in memory */
 
-void add_symbol(Symbols_Table *table, Symbols *symbol);
 
-/* Return 1 if the symbol is already in the table, 0 otherwise */
-int isPresent(Symbols_Table *table, Symbols *symbol);
+/* Return the size (in bytes) based on the type */
+int determine_size(Type type);
 
-/* Return 1 if s1 = s2, 0 otherwise */
-int isEqual(Symbols s1, Symbols s2);
+/* Return the string corresponding to the type */
+char* type_to_string(Type type);
 
-void print_table(Symbols_Table *table);
+/* Return the last adress of the symbol table */
+int get_last_adress(Symbols_Table* sym_table);
+
+/* Create a symbol */
+Symbol* make_symbol(char* ident, Type type, int adress_prev);
+
+/* Initialize a symbol table */
+Symbols_Table* init_Sym_table();
+
+/* Initialize a function table */ // Empty header and body
+Function_Table* init_Func_table(char* ident);
+
+/* Initialize a program table */
+Program_Table* init_Program_table();
+
+/* Free a symbol */
+void free_symbol(Symbol* symbol);
+
+/* Free a symbol table */
+void free_Sym_table(Symbols_Table* sym_table);
+
+/* Free a function table */
+void free_Func_table(Function_Table* func_table);
+
+/* Free a program table */
+void free_Program_table(Program_Table* prog_table);
+
+/* Add a symbol to the symbol table 
+ * we suppose that the table as enough left for the symbol
+*/
+void add_symbol(Symbols_Table* sym_table, Symbol* symbol);
+
+/* Verify if a symbol with the same ident is already in the table 
+ * Return 1 if the symbol is already in the table, 0 otherwise
+*/
+int isPresent(Symbols_Table* sym_table, Symbol* symbol);
+
+/* Print a symbol */
+void print_symbol(Symbol* symbol);
+
+/* Print a symbol table */
+void print_sym_table(Symbols_Table* sym_table);
+
+/* Print a function table */
+void print_func_table(Function_Table* func_table);
+
+/* Print a program table */
+void print_program_table(Program_Table* prog_table);
+
 
 #endif
